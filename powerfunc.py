a11bdefd266f3d8a3b18c48b1380e48178898857
Peter Donis <peterdonis@alum.mit.edu>
2011-12-15 16:21:41 -0500

    Added more documentation, tweaked convergence testing.

--- powerfunc.py
+++ powerfunc.py
@@ -14,6 +14,7 @@ but to actually use series to compute their corresponding
 functions, convergence is a critical issue.
 """
 
+from collections import deque
 from fractions import Fraction
 from itertools import islice
 
@@ -36,7 +37,8 @@ class PowerFunction(object):
     """
     
     error = Fraction(1, 10000)
-    
+    error_terms = 1
+    terms_max = 50
     ratio_max = 5
     
     def __init__(self, series):
@@ -57,9 +59,12 @@ class PowerFunction(object):
         significant figures (the ``figures`` argument controls this;
         internally, it is converted to an error tolerance equal to
         1 / 10^figures). In this case, ``DivergenceError`` is raised
-        if the series fails to converge; a number of different
-        convergence criteria are used, as described in the docstring
-        for the ``test_convergence`` method, below.
+        if the series fails to converge. The only other caveat is
+        that we only compute ``terms_max`` terms; this ensures that
+        we don't loop forever (or at least for a very long time) in
+        cases where we are close to the edge of the circle of
+        convergence (for example, computingthe tangent of a number
+        close to pi/2).
         
         If ``terms`` is provided, that number of terms is computed
         and their sum returned, regardless of convergence. (If a
@@ -79,32 +84,59 @@ class PowerFunction(object):
         elif error is None:
             error = self.error
         error = abs(error)
-        result = last = Fraction(0, 1)
+        result = Fraction(0, 1)
         self._clear_testfields()
         xt = Fraction(1, 1)
         for n, t in enumerate(self.__series):
+            if n >= self.terms_max:
+                break
             try:
                 term = t * xt
                 result += term
             except OverflowError:
                 raise DivergenceError("Series diverged to overflow point.")
             # This will raise DivergenceError if necessary
-            if self.test_convergence(x, n, term, last, result, error):
+            if self.converged(x, n, term, result, error):
                 break
-            last = term
             xt *= x
         return result
     
     def _clear_testfields(self):
         # Internal method to clear convergence testing fields
+        self.__terms_last = deque(maxlen=self.error_terms)
         self.__ratio_last = None  # last nonzero term for ratio test
         self.__ratio_count = 0  # number of times the ratio test has failed
     
-    def test_convergence(self, x, n, term, last, result, error):
+    def converged(self, x, n, term, result, error):
         """Test series convergence.
+        
+        Determining convergence is straightforward: we just check
+        that some number of terms (specified by the ``error_terms``
+        class field) change the result by less than ``error``.
+        
+        Determining divergence is more complicated, since we
+        don't have a symbolic representation of the function the
+        series is computing, we only have the terms of the series.
+        So the only classic test we can easily apply is the ratio
+        test: if the ratio of a term to the preceding (nonzero)
+        term is > 1 for some number of terms (determined by the
+        class field ``ratio_max``), we conclude that the series is
+        diverging. This is still not an analytical proof, of course;
+        but it is a reasonable heuristic, and it works for all of
+        the example series in ``powerseries.py``.
+        
+        Note that this definition of divergence also cannot take
+        account of periodic functions; our series representation
+        corresponds to a Taylor expansion about x = 0, so it only
+        converges within the circle of convergence centered on
+        that value. For example, computing the tangent of pi will
+        diverge, even though the proper analytical value of tan(pi)
+        is 0, because computing that would require us to expand the
+        series around x = pi.
         """
         if term != 0:
-            if abs(term) < (error * result):
+            self.__terms_last.append(term)
+            if abs(sum(self.__terms_last)) < (error * result):
                 return True
             if self.__ratio_last and ((term / self.__ratio_last) > 1):
                 self.__ratio_count += 1
