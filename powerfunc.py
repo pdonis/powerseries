b4c206f007b52b7798bd692b3b92c9f428e15b56
pdonis <peterdonis@alum.mit.edu>
2011-12-15 18:38:40 -0500

    Overflow now trapped and flagged as divergence in PowerFunction; added doctests for PowerFunction.

--- powerfunc.py
+++ powerfunc.py
@@ -11,7 +11,71 @@ arguments. In principle this is straightforward; we just
 sum the terms. However, the ``PowerSeries`` class represents
 series formally and does not deal at all with convergence;
 but to actually use series to compute their corresponding
-functions, convergence is a critical issue.
+functions, convergence is of course an issue. The
+``PowerFunction`` class in this module implements simple
+convergence testing; it also supports computing a fixed
+number of terms of the series, which it can do regardless
+of convergence issues.
+
+Test with the example functions from ``powerseries.py``:
+    
+    >>> from powerseries import *
+    >>> names = ['exp', 'sin', 'cos', 'tan', 'sec', 'arcsin', 'arctan', 'sinh', 'cosh', 'tanh', 'arcsinh', 'arctanh']
+    >>> for name in names:
+    ...     f = PowerFunction(globals()['%sseries' % name]())
+    ...     print name
+    ...     print "f(0)", f(0)
+    ...     print "f(1)", f(1)
+    ...     print "f(1) to 10 figures", f(1, figures=10)
+    ...
+    exp
+    f(0) 1
+    f(1) 685/252
+    f(1) to 10 figures 8463398743/3113510400
+    sin
+    f(0) 0
+    f(1) 305353/362880
+    f(1) to 10 figures 1100370038249/1307674368000
+    cos
+    f(0) 1
+    f(1) 4357/8064
+    f(1) to 10 figures 47102631757/87178291200
+    tan
+    f(0) 0
+    f(1) 303520407357844/194896477400625
+    f(1) to 10 figures 13462551943417438991627782493239751857561132563391/8644205195683235286768595007647709520704677734375
+    sec
+    f(0) 1
+    f(1) 900520175377937141/486580401635328000
+    f(1) to 10 figures 1209256320656611108711592273611327639089852593897960622737339/653363978554530140571699423545967107290604544983040000000000
+    arcsin
+    f(0) 0
+    f(1) 9934553633062508667548681803105637/6814870934827565564370797513932800
+    f(1) to 10 figures 9934553633062508667548681803105637/6814870934827565564370797513932800
+    arctan
+    f(0) 0
+    f(1) 77030060483083029083/96845140757687397075
+    f(1) to 10 figures 77030060483083029083/96845140757687397075
+    sinh
+    f(0) 0
+    f(1) 426457/362880
+    f(1) to 10 figures 1536780478171/1307674368000
+    cosh
+    f(0) 1
+    f(1) 6913/4480
+    f(1) to 10 figures 44841044309/29059430400
+    tanh
+    f(0) 0
+    f(1) 12517580680408876/16436269594119375
+    f(1) to 10 figures 140071833206259684360514733360266214425802526857/183919259482622027378055212928674670653291015625
+    arcsinh
+    f(0) 0
+    f(1) 6014169722227115002607630537270917/6814870934827565564370797513932800
+    f(1) to 10 figures 6014169722227115002607630537270917/6814870934827565564370797513932800
+    arctanh
+    f(0) 0
+    f(1) 250947670863258378883/96845140757687397075
+    f(1) to 10 figures 250947670863258378883/96845140757687397075
 """
 
 from collections import deque
@@ -26,10 +90,14 @@ class PowerFunction(object):
     """Wrap a power series and compute its function.
     
     A power series is represented as an iterable of coefficients; the
-    nth term is the coefficient of x**n. Uses this representation to
-    compute the value of F(x), where F is the function whose power
+    nth term is the coefficient of x**n. This representation is used
+    to compute the value of F(x), where F is the function whose power
     series is represented. Convergence testing is supported; an
-    exception is raised if a divergent value is requested.
+    exception is raised if a divergent value is requested. Computing
+    a fixed number of terms, regardless of convergence, is also
+    supported. ``DivergenceError`` is raised if a divergent value
+    is requested; divergence is detected by the convergence testing
+    code, or by an overflow occurring.
     
     Note that, although this class was written to work with the
     ``PowerSeries`` class, it can actually work with any iterable
@@ -67,9 +135,9 @@ class PowerFunction(object):
         close to pi/2).
         
         If ``terms`` is provided, that number of terms is computed
-        and their sum returned, regardless of convergence. (If a
-        divergent value is requested, an overflow may occur; no
-        checking is done to avoid this.)
+        and their sum returned, regardless of convergence. However,
+        if an overflow occurs before the requested number of terms
+        is computed, ``DivergenceError`` is raised.
         """
         if isinstance(x, (int, long)):
             x = Fraction(x, 1)
@@ -77,8 +145,8 @@ class PowerFunction(object):
             x = Fraction.from_float(x)
         if not isinstance(x, Fraction):
             raise ValueError("Power series function requires fraction as argument.")
-        if terms is not None:
-            return sum(t * (x**n) for n, t in enumerate(islice(self.__series, terms)))
+        if terms is None:
+            terms = self.terms_max
         if figures is not None:
             error = Fraction(1, 10**figures)
         elif error is None:
@@ -87,9 +155,7 @@ class PowerFunction(object):
         result = Fraction(0, 1)
         self._clear_testfields()
         xt = Fraction(1, 1)
-        for n, t in enumerate(self.__series):
-            if n >= self.terms_max:
-                break
+        for n, t in enumerate(islice(self.__series, terms)):
             try:
                 term = t * xt
                 result += term
@@ -132,7 +198,9 @@ class PowerFunction(object):
         that value. For example, computing the tangent of pi will
         diverge, even though the proper analytical value of tan(pi)
         is 0, because computing that would require us to expand the
-        series around x = pi.
+        series around x = pi. This is not seen as an issue because
+        the calling code can always apply an offset to x before
+        invoking the series function.
         """
         if term != 0:
             self.__terms_last.append(term)
@@ -144,3 +212,8 @@ class PowerFunction(object):
                 raise DivergenceError("Series failed ratio test.")
             self.__ratio_last = term
         return False
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
